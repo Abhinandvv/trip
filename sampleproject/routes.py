a0439196558723d42b4a11bb4fd8df9db8eae1cb
Alfin Jiji <53205930+alfinjiji@users.noreply.github.com>
2020-11-06 23:48:42 +0530

    modified header and login design

--- sampleproject/routes.py
+++ sampleproject/routes.py
@@ -6,21 +6,22 @@ from flask_login import login_user, current_user, logout_user
 # home and login route
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+    lform = LoginForm()
+    if lform.validate_on_submit():
+        user = User.query.filter_by(email=lform.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, lform.password.data):
+            login_user(user, remember=lform.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessfull. Please check email and password', 'danger') 
-    return render_template('index1.html', title='Home', form=form) 
+    return render_template('index1.html', title='Home', lform=lform) 
 # sign up route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
+    lform = LoginForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
@@ -28,7 +29,7 @@ def register():
         db.session.commit()
         flash(f'Account Created For {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('Signup.html', title='Signup', form=form)
+    return render_template('Signup.html', title='Signup', form=form, lform=lform)
 # logout route
 @app.route('/logout')
 def logout():
@@ -37,6 +38,7 @@ def logout():
 # add story
 @app.route('/addstory')
 def addstory():
+    lform = LoginForm()
     if current_user.is_authenticated:
-        return render_template('addstory.html', title='Add Story')
+        return render_template('addstory.html', title='Add Story', lform=lform)
     return redirect(url_for('home'))
